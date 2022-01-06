bafbe4d573e8413264e7a7dc341b000d87267ceb
Cameron Lund <cameronlund4@gmail.com>
2022-01-06 13:46:32 -0500

    Super basic info

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
-<div>
-  <p>
-    Trying out some CI/CD!
-  </p>
-  <Tutorial/>
-</div>
+  <div class="mx-auto p-10 md:p-20" style="text-align: center">
+    <h1 class="text-4xl md:text-8xl">
+      Cameron Lund
+    </h1>
+    <p class="text-xl md:text-4xl ease-in transition-all">
+      Infrastructure - Backend - Full Stack
+    </p>
+
+    <h2>
+      This is heavily WIP.
+    </h2>
+  </div>
 </template>
 
 <script>
