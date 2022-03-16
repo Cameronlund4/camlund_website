59d06b569544071b681ca78c3c3db3c99842fce8
Cameron Lund <cameronlund4@gmail.com>
2022-03-16 00:00:02 -0400

    Add some texture to background

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="" style="text-align: center">
-    <div class="flex flex-row w-full" style="background: #A2BDDD">
+    <div class="flex flex-row w-full header-texture">
       <div class="flex flex-col justify-center flex-1 w-full h-auto" style="color: #3E3C3C">
         <div>
           <div class="inline-block">
@@ -27,7 +27,7 @@
           </div>
         </div>
       </div>
-      <img src="~/assets/me_picture.jpeg" class="w-1/3 h-auto" style="clip-path: polygon(20% 0, 0 100%, 100% 100%, 100% 0);">
+      <img src="~/assets/cam_cactus.png" class="w-1/3 h-auto" style="clip-path: polygon(20% 0, 0 100%, 100% 100%, 100% 0);">
     </div>
 
     <div class="mx-auto p-10 md:p-20">
@@ -51,3 +51,17 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+<style>
+  .header-texture {
+    background:
+    radial-gradient(#8191b354 3px, transparent 4px),
+    radial-gradient(#8191b354 3px, transparent 4px),
+    linear-gradient(#A2BDDD 4px, transparent 0),
+    linear-gradient(45deg, transparent 74px, transparent 75px, #8191b354 75px, #8191b354 76px, transparent 77px, transparent 109px),
+    linear-gradient(-45deg, transparent 75px, transparent 76px, #8191b354 76px, #8191b354 77px, transparent 78px, transparent 109px),
+    #A2BDDD;
+    background-size: 109px 109px, 109px 109px,100% 6px, 109px 109px, 109px 109px;
+    background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+  }
+</style>
