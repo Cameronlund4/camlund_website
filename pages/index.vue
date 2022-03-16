507b0e5a62a9df3b9ab8801ad29f326e1a6ad34b
Cameron Lund <cameronlund4@gmail.com>
2022-03-15 22:57:48 -0400

    Try using webpack images

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,32 @@
   <div class="" style="text-align: center">
     <div class="flex flex-row w-full" style="background: #A2BDDD">
       <div class="flex flex-col justify-center flex-1 w-full h-auto" style="color: #3E3C3C">
-        <h1 class="text-4xl md:text-8xl">
-          Cameron Lund
-        </h1>
-        <p class="text-xl md:text-4xl ease-in transition-all">
-          Infrastructure - Backend - Full Stack
-        </p>
+        <div>
+          <h1 class="text-4xl md:text-8xl">
+            Cameron Lund
+          </h1>
+          <div class="inline-block">
+            <div class="flex flex-row w-auto justify-between">
+              <p class="text-xl md:text-4xl ease-in transition-all">
+                Infrastructure
+              </p>
+              <p class="text-xl md:text-4xl ease-in transition-all">
+                -
+              </p>
+              <p class="text-xl md:text-4xl ease-in transition-all">
+                Backend
+              </p>
+              <p class="text-xl md:text-4xl ease-in transition-all">
+                -
+              </p>
+              <p class="text-xl md:text-4xl ease-in transition-all">
+                Full Stack
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-      <img src="/me_picture.jpeg" class="w-1/3 h-auto" style="clip-path: polygon(20% 0, 0 100%, 100% 100%, 100% 0);">
+      <img src="~/assets/me_picture.jpeg" class="w-1/3 h-auto" style="clip-path: polygon(20% 0, 0 100%, 100% 100%, 100% 0);">
     </div>
 
     <div class="mx-auto p-10 md:p-20">
